c2b2fa3944d05614c812297fdd282a549e62322a
Xvariate <contact@xvariate.com>
2024-12-16 12:51:58 +0530

    Foundational setup completed!

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,36 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/prisma"
+import Credentials from "next-auth/providers/credentials"
+import * as z from "zod"
+import bcrypt from "bcryptjs"
+
+const SignInSchema = z.object({
+    email: z.string().email({ message: "Email is required" }),
+    password: z.string().min(1, { message: "Password is required" }),
+    code: z.optional(z.string()),
+})
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
-    providers: [],
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = SignInSchema.safeParse(credentials)
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data
+                    const user = await prisma.user.findUnique({
+                        where: {
+                            email,
+                        }
+                    })
+
+                    if (!user || !user.password) return null;
+                    const passwordMatch = await bcrypt.compare(password, user.password)
+                    if (passwordMatch) return user;
+                }
+                return null;
+            }
+        })
+    ],
 })
